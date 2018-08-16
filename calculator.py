4ed315ba91f4072e27a42432897c7537893abb88
AlexTheGreat4000 <AlexTheGreat4000@users.noreply.github.com>
2018-08-16 14:59:22 +1000

    Modifed answer output

--- calculator.py
+++ calculator.py
@@ -1,5 +1,17 @@
 print('Welcome to Calculator')
 
+def add(x, y):
+        return x + y
+
+def subtract(x, y):
+        return x - y
+
+def multiply(x, y):
+        return x * y
+
+def divide(x, y):
+        return x / y
+
 print('Operation.')
 print('1.Add')
 print('2.Subtract')
@@ -12,20 +24,16 @@ operation = input('Select an option: ')
     number_2 = int(input('Enter your second number: '))
 
 if operation == '1':
-    print('{} + {} = '.format(number_1, number_2))
-    print(number_1 + number_2)
+        print(number_1, "+", number_2, "=", add(number_1, number_2))
 
 elif operation == '2':
-    print('{} - {} = '.format(number_1, number_2))
-    print(number_1 - number_2)
+        print(number_1, '-', number_2, '=', subtract(number_1, number_2))
 
 elif operation == '3':
-    print('{} * {} = '.format(number_1, number_2))
-    print(number_1 * number_2)
+        print(number_1, '*', number_2, '=', multiply(number_1, number_2))
 
 elif operation == '4':
-    print('{} / {} = '.format(number_1, number_2))
-    print(number_1 / number_2)
+        print(number_1, '/', number_2, '=', divide(number_1, number_2))
 
 else:
     print('You have not typed a valid operator, please run the program again.')
